f94fecefd97f89e49d017e71398ec00f432277f3
Никита Минаков <144937189+GotMeIkari@users.noreply.github.com>
2023-09-24 15:41:39 +0300

    Update Source.c

--- Project1/Source.c
+++ Project1/Source.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
-
-int main() {
-    puts("     |\\");
-    puts("     | \\");
-    puts("     |  \\");
-    puts("     |   \\");
-    puts("     |    \\");
-    puts("     |     \\");
-    puts("     |______\\");
-    puts("     |");
-    puts("_____|_______");
-    puts("\\___________/");
-
+#include<stdio.h>
+#include<locale.h>
+ 
+int main()
+{
+    int L = 2333;
+    float n = 3;
+    int k = 3;
+    int m = 5;
+    setlocale(LC_ALL, "RUS");
+    
+    printf("%4.0f\n%5d\n__________\n\n%0+10.5f", n, L, n / L);
+    
     return 0;
 }
