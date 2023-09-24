a03a495cefb649d608ca169568922a543b622e26
Никита Минаков <144937189+GotMeIkari@users.noreply.github.com>
2023-09-24 15:47:32 +0300

    Update SourceTwo.c

--- Project1/SourceTwo.c
+++ Project1/SourceTwo.c
@@ -1,1 +1,19 @@
+#include<stdio.h>
+#include<locale.h>
 
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int a, b;
+    
+    printf("Введите a и b: ");
+    scanf("%d %d", &a, &b);
+    
+    printf("___________________\n");
+    printf("| a*b | a+b | a-b |\n");
+    printf("-------------------\n");
+    printf("|%2d*%-2d|%2d+%-2d|%2d-%-2d|\n", a, b, a, b, a, b);
+    printf("-------------------\n");
+    printf("|%3d  |%3d  |%3d  |\n", a * b, a + b, a - b);
+    printf("-------------------\n");
+}
